17110b50a28b0738a137021b9b9f848722617911
Lohana Torres <lt.leopoldo@gmail.com>
2021-02-25 17:10:56 -0300

    fix: port bug

--- src/config/database.js
+++ src/config/database.js
@@ -2,15 +2,16 @@
 require('dotenv').config();
 
 module.exports = {
-  dialect: 'postgres',    /* Selected database */
+  dialect: 'postgres' /* Selected database */,
   host: process.env.DATABASE_HOST || 'localhost',
-  port: process.env.DATABASE_PORT || '5432', /* Database port */
-  username: process.env.DATABASE_USERNAME || 'postgres',   /* Your Image name */
-  password: process.env.DATABASE_PWD || 'postgres',   /* Your container password */
-  database: process.env.DATABASE || 'codefiq',    /* Desired name */
-  define: {               /* Standards */
-    timestamps: true,
+  port: process.env.DATABASE_PORT || '5433' /* Database port */,
+  username: process.env.DATABASE_USERNAME || 'postgres' /* Your Image name */,
+  password:
+    process.env.DATABASE_PWD || 'postgres' /* Your container password */,
+  database: process.env.DATABASE || 'codefiq' /* Desired name */,
+  define: {
+    /* Standards */ timestamps: true,
     underscored: true,
     underscoredAll: true,
   },
-}
+};
