c67da2aaf470c259ad7238051cef7aef9850d036
Arlene Pelenda <arlenej011@gmail.com>
2021-03-23 15:08:31 -0300

    Add ssl database

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,12 @@ require('dotenv').config();
 
 module.exports = {
   dialect: 'postgres' /* Selected database */,
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      //rejectUnauthorized: false // This line will fix new error
+    }
+  },
   host: process.env.DATABASE_HOST || 'localhost',
   port: process.env.DATABASE_PORT || '5433' /* Database port */,
   username: process.env.DATABASE_USERNAME || 'postgres' /* Your Image name */,
